352031e54fb398cc00f650d1e763bbe5482b38b6
Christian Câmara de Abreu <christian@MacBook-Air-de-Christian.local>
2021-03-03 18:53:21 -0300

    mega sena sorteia numeros

--- src/components/mega/Mega.js
+++ src/components/mega/Mega.js
@@ -1,14 +1,38 @@
 import React, {Component} from 'react'
-import {Text, TextInput} from 'react-native'
+import {Text, TextInput, Button} from 'react-native'
 
 export default class Mega extends Component{
     state ={
-        qtdNumeros : this.props.qtdNumeros
+        qtdNumeros : this.props.qtdNumeros,
+        numeros : []
     }
 
     alterarQtdNumeros = (qt) => {
-        this.setState({ qtdNumeros : qt})
+        this.setState({ qtdNumeros : +qt})
     }
+
+    sortearNumero(lista){
+        let n = 0;
+        do {
+            n = parseInt(Math.random() * 60 + 1);
+        }while(lista.includes(n) );
+        return n;
+    }
+
+
+    gerarNumeros = ()=>{
+        let lista = [];
+        for(i=0;i<=this.state.qtdNumeros;i++){
+            lista[i]=this.sortearNumero(lista);
+        }
+        lista.sort(function(a,b){return a-b;});
+        this.setState({numeros : lista});
+    }
+
+    clickSortear = ()=> {
+        this.gerarNumeros();       
+    }
+
 
     render(){
         return (
@@ -18,9 +42,11 @@ export default class Mega extends Component{
                 keyboardType={'numeric'}
                 style={{borderBottomWidth:1}}
                 placeholder="Informar valor"
-                value={this.state.qtdNumeros}
+                value={`${this.state.qtdNumeros}`}
                 onChangeText={this.alterarQtdNumeros}                               
             />
+            <Button title='Sortear!' onPress={this.clickSortear} />
+            <Text>{this.state.numeros.join(' - ')}</Text>
             </>
         )
     }
